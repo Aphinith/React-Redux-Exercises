676fe7afd8fd42f65c6b0607c92ff60f2b598c3c
Aralya Phinith <aphinith84@gmail.com>
2016-12-06 14:02:51 -0800

    add form key to rootReducer

--- Blog_Practice/src/components/posts_new.js
+++ Blog_Practice/src/components/posts_new.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
 
   render() {
+
+    const { fields: {title, categories, content }, handleSubmit } = this.props;
+    console.log(title);
+
     return (
-      <div>Create form</div>
+      <form onSubmit={handleSubmit} >
+        <h3>Create a new Post</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>Content</label>
+          <textarea className="form-control" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content']
+})(PostsNew);

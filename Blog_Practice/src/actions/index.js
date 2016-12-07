519253d0ed3f86a9ce2934446da581bffd5ccd7a
Aralya Phinith <aphinith84@gmail.com>
2016-12-06 20:27:32 -0800

    render details of each post and create link back to index

--- Blog_Practice/src/actions/index.js
+++ Blog_Practice/src/actions/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
-export const CREATE_POST = "CREATE_POST";
+export const CREATE_POST = 'CREATE_POST';
+export const FETCH_POST = 'FETCH_POST'
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=123qwe';
@@ -14,7 +15,7 @@ export function fetchPosts() {
     type: FETCH_POSTS,
     payload: request
   };
-}
+};
 
 export function createPost(props) {
 
@@ -24,4 +25,14 @@ export function createPost(props) {
     type: CREATE_POST,
     payload: request
   }
-}
+};
+
+export function fetchPost(id) {
+
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: FETCH_POST,
+    payload: request
+  }
+};
--- Blog_Practice/src/components/posts_show.js
+++ Blog_Practice/src/components/posts_show.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsShow extends Component { 
 
+  componentWillMount() {
+    this.props.fetchPost(this.props.params.id);
+  }
+
   render() {
+
+    if (!this.props.post) {
+      return <div>Loading...</div>
+    };
+
     return (
       <div>
-        Show post {this.props.params.id}
+        <Link to="/">Back to Index</Link>
+        <h3>{this.props.post.title}</h3>
+        <h6>Categories: {this.props.post.categories}</h6>
+        <p>{this.props.post.content}</p>
       </div>
     );
   }
 }
 
-export default PostsShow;
+function mapStateToProps(state) {
+  return { post: state.posts.post}
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);

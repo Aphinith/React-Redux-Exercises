c81d98ad45e73731930f85a5e3edc46d7f6ab92d
Aralya Phinith <aphinith84@gmail.com>
2016-12-06 20:38:00 -0800

    create delete button, deletes post

--- Blog_Practice/src/components/posts_show.js
+++ Blog_Practice/src/components/posts_show.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions/index';
+import { fetchPost, deletePost } from '../actions/index';
 import { Link } from 'react-router';
 
 class PostsShow extends Component { 
 
   componentWillMount() {
     this.props.fetchPost(this.props.params.id);
+  }
+
+  onDeleteClick() {
+    this.props.deletePost(this.props.params.id);
   }
 
   render() {
@@ -18,6 +22,11 @@ class PostsShow extends Component {
     return (
       <div>
         <Link to="/">Back to Index</Link>
+        <button 
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}>
+          Delete Post
+        </button>
         <h3>{this.props.post.title}</h3>
         <h6>Categories: {this.props.post.categories}</h6>
         <p>{this.props.post.content}</p>
@@ -30,4 +39,4 @@ function mapStateToProps(state) {
   return { post: state.posts.post}
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
